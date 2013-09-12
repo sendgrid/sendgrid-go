4baf02f974755e2442ef59c8f163e669b649c2c6
elbuo8 <yamil.asusta@upr.edu>
2013-09-11 22:57:38 -0400

    Fully working version with multiple emails, encoding etc

--- sendgrid-web.go
+++ sendgrid-web.go
@@ -1,12 +1,11 @@
 package sendgridgo
 
 import (
-	//"errors"
-	"net/http"
-	//"net/url"
-	//"encoding/json"
+	"bytes"
+	"errors"
 	"fmt"
-	"net/mail"
+	"net/http"
+	"net/url"
 )
 
 type SGClient struct {
@@ -20,20 +19,45 @@ func NewSendGridClient(apiUser, apiPwd string) SGClient {
 }
 
 func (sg *SGClient) Send(m Mail) error {
-	url := fmt.Sprintf("%sapi_user=%s&api_key=%s&to=%s&subject=%s&html=%s&from=%s", sg.apiUrl, sg.apiUser, sg.apiPwd, m.to, m.subject, m.html, m.from)
-	fmt.Println(url)
-	r, e := http.Get(url)
-	fmt.Println(r, e)
-	return nil
+	if total := len(m.to); total == len(m.toname) || total == 1 {
+		var reqUrl bytes.Buffer
+		reqUrl.WriteString(sg.apiUrl)
+		values := url.Values{}
+		values.Set("api_user", sg.apiUser)
+		values.Set("api_key", sg.apiPwd)
+		values.Set("subject", m.subject)
+		values.Set("html", m.html)
+		values.Set("from", m.from)
+		switch {
+		case total == 1:
+			values.Set("to", m.to[0])
+			if len(m.toname) == 1 {
+				values.Set("toname", m.toname[0])
+			}
+		case total > 1:
+			for i := 0; i < total; i++ {
+				values.Set("to[]", m.to[i])
+				values.Set("toname[]", m.toname[i])
+			}
+		}
+		fmt.Println(reqUrl.String(), values)
+		reqUrl.WriteString(values.Encode())
+		fmt.Println(reqUrl)
+		r, e := http.Get(reqUrl.String())
+		fmt.Println(r, e)
+		return nil
+	}
+	return errors.New(`The total names of receipients must be equal
+	to the email addresses. Unless there is only one receipient.`)
 }
 
 type Mail struct {
-	to       string
-	toname   string
+	to       []string
+	toname   []string
 	subject  string
 	html     string
 	from     string
-	bcc      string
+	bcc      []string
 	fromname string
 	replyto  string
 	date     string
@@ -48,11 +72,11 @@ func NewMail() Mail {
 TODO: Validate email addressed with RegExp.
 */
 func (m *Mail) AddTo(email string) {
-	m.to = email
+	m.to = append(m.to, email)
 }
 
 func (m *Mail) AddToName(name string) {
-	m.toname = name
+	m.toname = append(m.toname, name)
 }
 
 func (m *Mail) AddSubject(s string) {
@@ -68,7 +92,7 @@ func (m *Mail) AddFrom(from string) {
 }
 
 func (m *Mail) AddBCC(email string) {
-	m.bcc = email
+	m.bcc = append(m.bcc, email)
 }
 
 func (m *Mail) AddFromName(name string) {
