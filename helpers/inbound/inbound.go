2513f697acee93ffbb19228129aa305f2d848e11
Elmer Thomas <elmer@thinkingserious.com>
2016-09-29 15:17:36 -0700

    Raw parsing now works

--- helpers/inbound/inbound.go
+++ helpers/inbound/inbound.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -12,22 +13,22 @@ import (
 	"strings"
 )
 
-type inbound_parse struct {
+type inboundParse struct {
 	Test1 string
 	Test2 string
 }
 
-type Configuration struct {
+type configuration struct {
 	Endpoint string `json:"endpoint"`
 	Port     string `json:"port"`
 }
 
-func LoadConfig(path string) Configuration {
+func loadConfig(path string) configuration {
 	file, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Fatal("Config File Missing. ", err)
 	}
-	var conf Configuration
+	var conf configuration
 	err = json.Unmarshal(file, &conf)
 	if err != nil {
 		log.Fatal("Config Parse Error: ", err)
@@ -52,7 +53,7 @@ func inboundHandler(w http.ResponseWriter, r *http.Request) {
 		emailHeader := make(map[string]string)
 		for {
 			p, err := mr.NextPart()
-			// We have found an attachment
+			// We have found an attachment, binary data
 			if err == nil && p.FileName() != "" {
 				fmt.Println("FileName: ", p.FileName())
 				contentType := p.Header.Get("Content-Type")
@@ -90,9 +91,9 @@ func inboundHandler(w http.ResponseWriter, r *http.Request) {
 				header = header[17 : len(header)-1]
 				parsedEmail[header] = string(value)
 			} else {
-				header = header[11 :]
+				header = header[11:]
 				f := strings.Split(header, "=")
-				parsedEmail[f[1][1:len(f[1])-11]] = f[2][1:len(f[2])-1]
+				parsedEmail[f[1][1:len(f[1])-11]] = f[2][1 : len(f[2])-1]
 			}
 			if header == "headers" {
 				s := strings.Split(string(value), "\n")
@@ -106,12 +107,73 @@ func inboundHandler(w http.ResponseWriter, r *http.Request) {
 				}
 			}
 			delete(parsedEmail, "headers")
+
+			// We have a raw message
+			if header == "email" {
+				body := strings.NewReader(string(value))
+				bodySplit := strings.Split(string(value), "Content-Type: multipart/mixed; ")
+				scanner := bufio.NewScanner(strings.NewReader(bodySplit[1]))
+				var lines []string
+				for scanner.Scan() {
+					lines = append(lines, scanner.Text())
+					break
+				}
+				//fmt.Println("Split:", bodySplit[0])
+				//fmt.Println("Split:", bodySplit[1])
+				boundary := lines[0][9:]
+				//fmt.Println("Split:", params["boundary"])
+
+				// Get the attachments, base64 encoded
+				raw := multipart.NewReader(body, boundary)
+				for {
+					q, err := raw.NextPart()
+					if err == io.EOF {
+						// We have finished parsing
+						break
+					}
+					v, err := ioutil.ReadAll(q)
+					if err != nil {
+						log.Fatal(err)
+					}
+					header := q.Header.Get("Content-Type")
+
+					// Parse the headers
+					if strings.Contains(header, "multipart/alternative") {
+						body := strings.NewReader(string(value))
+						bodySplit := strings.Split(string(value), "Content-Type: multipart/alternative; ")
+						scanner := bufio.NewScanner(strings.NewReader(bodySplit[1]))
+						var lines []string
+						for scanner.Scan() {
+							lines = append(lines, scanner.Text())
+							break
+						}
+						boundary := lines[0][9:]
+						raw := multipart.NewReader(body, boundary)
+						for {
+							q, err := raw.NextPart()
+							if err == io.EOF {
+								// We have finished parsing
+								break
+							}
+							v, err := ioutil.ReadAll(q)
+							if err != nil {
+								log.Fatal(err)
+							}
+							header := q.Header.Get("Content-Type")
+							fmt.Println("iEmail:", header, "iValue:", string(v))
+						}
+					} else {
+						fmt.Println("Email:", header, "Value:", string(v))
+					}
+				}
+			}
+			delete(parsedEmail, "email")
 		}
 	}
 }
 
 func main() {
-	conf := LoadConfig("./conf.json")
+	conf := loadConfig("./conf.json")
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc(conf.Endpoint, inboundHandler)
 	http.ListenAndServe(conf.Port, nil)
