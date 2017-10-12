2a4300add919b1d54ba41c8999d5ab5344b50d93
Andy Trimble <andy.trimble@gmail.com>
2017-10-11 18:34:00 -0600

    Added optional rate limit handling.

--- sendgrid.go
+++ sendgrid.go
@@ -1,10 +1,20 @@
 // Package sendgrid provides a simple interface to interact with the SendGrid API
 package sendgrid
 
-import "github.com/sendgrid/rest" // depends on version 2.2.0
+import (
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/sendgrid/rest"
+) // depends on version 2.2.0
 
 // Version is this client library's current version
-const Version = "3.1.0"
+const (
+	Version        = "3.1.0"
+	rateLimitRetry = 5
+	rateLimitSleep = 1100
+)
 
 // GetRequest returns a default request object.
 func GetRequest(key string, endpoint string, host string) rest.Request {
@@ -29,4 +39,38 @@ var DefaultClient = rest.DefaultClient
 // API sets up the request to the SendGrid API, this is main interface.
 func API(request rest.Request) (*rest.Response, error) {
 	return DefaultClient.API(request)
+}
+
+// Request attempts a request asynchronously in a new go
+// routine. This function returns two channels: responses
+// and errors. This function will retry in the case of a
+// rate limit.
+func Request(request rest.Request) (chan *rest.Response, chan error) {
+	r := make(chan *rest.Response)
+	e := make(chan error)
+
+	go func() {
+		retry := 0
+		for {
+			response, err := DefaultClient.API(request)
+			if err != nil {
+				e <- err
+				return
+			}
+
+			if response.StatusCode == http.StatusTooManyRequests {
+				if retry > rateLimitRetry {
+					e <- errors.New("Rate limit retry exceeded")
+					return
+				}
+				retry++
+				time.Sleep(rateLimitSleep * time.Millisecond)
+				continue
+			}
+
+			r <- response
+		}
+	}()
+
+	return r, e
 }
