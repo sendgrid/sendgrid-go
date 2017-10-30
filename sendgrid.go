b5783423a8d8489084640cebf887322ba3717d9a
mbernier <mkbernier@gmail.com>
2017-10-29 21:08:40 -0600

    not =, :

--- sendgrid.go
+++ sendgrid.go
@@ -22,9 +22,9 @@ func GetRequest(key string, endpoint string, host string) rest.Request {
 	}
 	baseURL := host + endpoint
 	requestHeaders := map[string]string{
-		requestHeaders["Authorization"] = "Bearer " + key,
-		requestHeaders["User-Agent"] = "sendgrid/" + Version + ";go",
-		requestHeaders["Accept"] = "application/json",
+		requestHeaders["Authorization"]: "Bearer " + key,
+		requestHeaders["User-Agent"]: "sendgrid/" + Version + ";go",
+		requestHeaders["Accept"]: "application/json",
 	}
 	request := rest.Request{
 		BaseURL: baseURL,
