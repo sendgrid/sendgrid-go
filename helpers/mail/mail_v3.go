775cc0ea6048ec81834e386ad5b8b625fe134374
NafisFaysal <nafis@furqansoftware.com>
2017-10-30 19:16:29 +0600

    clean some code

--- helpers/mail/mail_v3.go
+++ helpers/mail/mail_v3.go
@@ -2,24 +2,7 @@ package mail
 
 import (
 	"encoding/json"
-	"flag"
-	"fmt"
 	"log"
-
-	"github.com/ttacon/box"
-	"github.com/ttacon/pretty"
-	"golang.org/x/oauth2"
-)
-
-var (
-	clientId     = flag.String("cid", "", "OAuth Client ID")
-	clientSecret = flag.String("csec", "", "OAuth Client Secret")
-
-	accessToken  = flag.String("atok", "", "Access Token")
-	refreshToken = flag.String("rtok", "", "Refresh Token")
-
-	fileName = flag.String("file", "", "File to upload")
-	fileId   = flag.String("fid", "", "File (ID) to grab")
 )
 
 // SGMailV3 contains mail struct
@@ -352,87 +335,6 @@ func (a *Attachment) SetDisposition(disposition string) *Attachment {
 func (a *Attachment) SetContentID(contentID string) *Attachment {
 	a.ContentID = contentID
 	return a
-}
-
-func (s *SGMailV3) UploadFromBox(a ...Attachment) {
-	flag.Parse()
-
-	if len(*clientId) == 0 || len(*clientSecret) == 0 ||
-		len(*accessToken) == 0 || len(*refreshToken) == 0 ||
-		len(*fileName) == 0 {
-		fmt.Println("unfortunately all flags must be provided")
-		return
-	}
-
-	// Set our OAuth2 configuration up
-	var (
-		configSource = box.NewConfigSource(
-			&oauth2.Config{
-				ClientID:     *clientId,
-				ClientSecret: *clientSecret,
-				Scopes:       nil,
-				Endpoint: oauth2.Endpoint{
-					AuthURL:  "https://app.box.com/api/oauth2/authorize",
-					TokenURL: "https://app.box.com/api/oauth2/token",
-				},
-				RedirectURL: "http://localhost:8080/handle",
-			},
-		)
-		tok = &oauth2.Token{
-			TokenType:    "Bearer",
-			AccessToken:  *accessToken,
-			RefreshToken: *refreshToken,
-		}
-		c = configSource.NewClient(tok)
-	)
-
-	resp, folder, err := c.FileService().UploadFile(*fileName, "0")
-	fmt.Println("resp: ", resp)
-	fmt.Println("err: ", err)
-	pretty.Print(folder)
-
-	// Print out the new tokens for next time
-	fmt.Printf("%#v\n", tok)
-}
-func (s *SGMailV3) DownloadAttachedBoxFile(a ...Attachment) {
-	flag.Parse()
-
-	if len(*clientId) == 0 || len(*clientSecret) == 0 ||
-		len(*accessToken) == 0 || len(*refreshToken) == 0 ||
-		len(*fileName) == 0 {
-		fmt.Println("unfortunately all flags must be provided")
-		return
-	}
-
-	// Set our OAuth2 configuration up
-	var (
-		configSource = box.NewConfigSource(
-			&oauth2.Config{
-				ClientID:     *clientId,
-				ClientSecret: *clientSecret,
-				Scopes:       nil,
-				Endpoint: oauth2.Endpoint{
-					AuthURL:  "https://app.box.com/api/oauth2/authorize",
-					TokenURL: "https://app.box.com/api/oauth2/token",
-				},
-				RedirectURL: "http://localhost:8080/handle",
-			},
-		)
-		tok = &oauth2.Token{
-			TokenType:    "Bearer",
-			AccessToken:  *accessToken,
-			RefreshToken: *refreshToken,
-		}
-		c = configSource.NewClient(tok)
-	)
-
-	resp, folder, err := c.FileService().UploadFile(*fileName, "0")
-	fmt.Println("resp: ", resp)
-	fmt.Println("err: ", err)
-	pretty.Print(folder)
-
-	// Print out the new tokens for next time
-	fmt.Printf("%#v\n", tok)
 }
 
 func NewASM() *Asm {
