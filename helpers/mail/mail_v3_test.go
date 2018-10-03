3bd52f7527fb34152ef78ee364a4f3aafe978313
arthur silva <arxdsilva@gmail.com>
2018-10-03 09:34:23 -0300

    helpers/mail: add testify to new test

--- helpers/mail/mail_v3_test.go
+++ helpers/mail/mail_v3_test.go
@@ -687,21 +687,10 @@ func TestV3NewSingleEmailWithEmptyHTMLContent(t *testing.T) {
 	m, _ := json.Marshal(message)
 	fmt.Println(string(m))
 
-	if message == nil {
-		t.Errorf("NewV3MailInit() shouldn't return nil")
-	}
-
-	if message.From == nil {
-		t.Errorf("From shouldn't be nil")
-	}
-
-	if message.Subject != subject {
-		t.Errorf("Subject should be %s, got %s", subject, message.Subject)
-	}
-
-	if message.Content == nil {
-		t.Errorf("Content shouldn't be nil")
-	}
+	assert.NotNil(t, message, "NewV3MailInit() shouldn't return nil")
+	assert.NotNil(t, message.From, "From shouldn't be nil")
+	assert.Equal(t, message.Subject, subject, fmt.Sprintf("Subject should be %s, got %s", subject, message.Subject))
+	assert.NotNil(t, message.Content, "Content shouldn't be nil")
 }
 
 func TestV3NewClickTrackingSetting(t *testing.T) {
