b75092267e0760467d991879e2be41639e88bf33
Frederik Creemers <frederikcreemers@gmail.com>
2018-10-13 01:56:03 +0200

    Create GetBrandedLinks function

--- helpers/whitelabel/branded_links.go
+++ helpers/whitelabel/branded_links.go
@@ -23,23 +23,40 @@ type BrandedLink struct {
 	DNS       map[string]DNSEntry `json:"dns"`
 }
 
+const linksEndpoint = "/v3/whitelabel/links"
+
 // CreateBrandedLink adds a new domain for branded links. For more info, see: https://sendgrid.com/docs/API_Reference/api_v3.html
-func CreateBrandedLink(key string, sub SubdomainInfo, isDefault bool) (BrandedLink, error) {
-	cl := sendgrid.NewClientForEndpoint(key, "/v3/whitelabel/links")
+func CreateBrandedLink(key string, sub SubdomainInfo, isDefault bool) (*BrandedLink, error) {
+	cl := sendgrid.NewClientForEndpoint(key, linksEndpoint)
 	var err error
 	var link = BrandedLink{}
 
 	cl.Body, err = json.Marshal(brandedLinkRequest{sub, isDefault})
 	if err != nil {
-		return link, err
+		return &link, err
 	}
 
 	cl.Method = "POST"
 	resp, err := sendgrid.MakeRequestRetry(cl.Request)
 	if err != nil {
-		return link, err
+		return &link, err
 	}
 
 	err = json.Unmarshal([]byte(resp.Body), &link)
-	return link, err
+	return &link, err
+}
+
+// GetBrandedLinks fetches all branded link domains.
+func GetBrandedLinks(key string) ([]BrandedLink, error) {
+	cl := sendgrid.NewClientForEndpoint(key, linksEndpoint)
+	cl.Method = "GET"
+
+	resp, err := sendgrid.MakeRequestRetry(cl.Request)
+	if err != nil {
+		return nil, err
+	}
+
+	var links []BrandedLink
+	err = json.Unmarshal([]byte(resp.Body), &links)
+	return links, err
 }
