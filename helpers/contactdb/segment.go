910c94ecf0f75dffa38fbc9a58c8b782f13a136f
Omar A. Hachach <ohachach@gmail.com>
2018-10-24 20:34:42 +0200

    Finish segment helpers.

--- helpers/contactdb/segment.go
+++ helpers/contactdb/segment.go
@@ -1,17 +1,28 @@
 package contactdb
 
+import (
+	"encoding/json"
+	"strconv"
+)
+
 // Segment holds the struct for the segments
 type Segment struct {
 	ID             int          `json:"id,omitempty"`
 	Name           string       `json:"name,omitempty"`
 	ListID         int          `json:"list_id,omitempty"`
 	Conditions     []*Condition `json:"conditions,omitempty"`
-	AndOr          string       `json:"and_or"` // No omitempty because an empty string is a valid value.
 	RecipientCount int          `json:"recipient_count,omitempty"`
+}
+
+// segmentList is a list of Segments.
+// Used for unmarshalling.
+type segmentList struct {
+	Segments []*Segment `json:"segments,omitempty"`
 }
 
 // Condition is a condition
 type Condition struct {
+	AndOr    string            `json:"and_or"`
 	Field    string            `json:"field,omitempty"`
 	Value    string            `json:"value,omitempty"`
 	Operator ConditionOperator `json:"operator,omitempty"`
@@ -31,30 +42,103 @@ const (
 
 // NewSegment creats a new segment. (POST)
 func NewSegment(listID int, conditions []*Condition, name, apiKey string) (*Segment, error) {
-	return nil, nil
+	payload, err := json.Marshal(Segment{
+		ListID:     listID,
+		Conditions: conditions,
+		Name:       name,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := SendPOSTRequest(apiKey, "/contactdb/segments", string(payload))
+	if err != nil {
+		return nil, err
+	}
+
+	segment := &Segment{}
+	err = json.Unmarshal(response, segment)
+	if err != nil {
+		return nil, err
+	}
+
+	return segment, nil
 }
 
 // GetSegment will return a specific segment by ID. (GET)
-func GetSegment(segID int, apiKey string) (*Segment, error) {
-	return nil, nil
+func GetSegment(segID, apiKey string) (*Segment, error) {
+	response, err := SendGETRequest(apiKey, "/contactdb/segments/"+segID+"?segment_id="+segID)
+	if err != nil {
+		return nil, err
+	}
+
+	segment := &Segment{}
+	err = json.Unmarshal(response, segment)
+	if err != nil {
+		return nil, err
+	}
+
+	return segment, nil
 }
 
 // GetAllSegments will return all of your segments. (GET)
 func GetAllSegments(apiKey string) ([]*Segment, error) {
-	return nil, nil
+	response, err := SendGETRequest(apiKey, "/contactdb/segments")
+	if err != nil {
+		return nil, err
+	}
+
+	segments := &segmentList{}
+	err = json.Unmarshal(response, segments)
+	if err != nil {
+		return nil, err
+	}
+
+	return segments.Segments, nil
 }
 
 // UpdateSegment will update a specific segment with new values. (PATCH)
-func UpdateSegment(segID int, segment *Segment, apiKey string) (*Segment, error) {
-	return nil, nil
+func UpdateSegment(segment *Segment, segID, apiKey string) (*Segment, error) {
+	payload, err := json.Marshal(segment)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := SendPATCHRequest(apiKey, "/contactdb/segments/"+segID, string(payload))
+	if err != nil {
+		return nil, err
+	}
+
+	newSeg := &Segment{}
+	err = json.Unmarshal(response, newSeg)
+	if err != nil {
+		return nil, err
+	}
+
+	return newSeg, nil
 }
 
 // DeleteSegment will delete a specific segment. (DELETE)
-func DeleteSegment(segID int, apiKey string) error {
-	return nil
+// DeleteContacts: True to delete all contacts matching the segment in
+// addition to deleting the segment.
+func DeleteSegment(deleteContacts bool, segID, apiKey string) error {
+	path := "/contactdb/segments/" + segID + "?delete_contacts=" + strconv.FormatBool(deleteContacts)
+	_, err := SendDELETERequest(apiKey, path, "")
+	return err
 }
 
 // GetSegmentRecipients will return all of the recipients of a segment. (GET)
 func GetSegmentRecipients(segID, apiKey string) ([]*Recipient, error) {
-	return nil, nil
+	response, err := SendGETRequest(apiKey, "/contactdb/segments/"+segID+"/recipients")
+	if err != nil {
+		return nil, err
+	}
+
+	recipients := &recipientList{}
+	err = json.Unmarshal(response, recipients)
+	if err != nil {
+		return nil, err
+	}
+
+	return recipients.Recipients, nil
 }
