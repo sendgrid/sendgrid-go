f78241743a7d69c62ffc933f3e778f3a0175b0af
brpat07 <20097156+brpat07@users.noreply.github.com>
2020-05-05 22:59:17 -0700

    added helper function to verify the signature

--- helpers/securewebhook/securewebhook.go
+++ helpers/securewebhook/securewebhook.go
@@ -1,12 +1,23 @@
 package securewebhook
 
 import (
+	"crypto/ecdsa"
+	"crypto/sha256"
+	"encoding/asn1"
+	"encoding/base64"
 	"encoding/json"
+	"math/big"
 )
 
 // Settings ...
 type Settings struct {
 	Enable *bool `json:"enabled,omitempty"`
+}
+
+// RS ...
+type RS struct {
+	R *big.Int
+	S *big.Int
 }
 
 // NewSettings ...
@@ -26,4 +37,26 @@ func GetRequestBody(s *Settings) ([]byte, error) {
 		return nil, err
 	}
 	return b, nil
+}
+
+// VerifySignature ...
+func VerifySignature(publicKey *ecdsa.PublicKey, payload []byte, signature, timestamp string) (bool, error) {
+	signatureBytes, err := base64.StdEncoding.DecodeString(signature)
+	if err != nil {
+		return false, err
+	}
+
+	ecdsaSig := &RS{}
+	_, err = asn1.Unmarshal(signatureBytes, ecdsaSig)
+	if err != nil {
+		return false, err
+	}
+
+	h := sha256.New()
+	if timestamp != "" {
+		h.Write([]byte(timestamp))
+	}
+	h.Write(payload)
+
+	return ecdsa.Verify(publicKey, h.Sum(nil), ecdsaSig.R, ecdsaSig.S), nil
 }
--- helpers/securewebhook/securewebhook_test.go
+++ helpers/securewebhook/securewebhook_test.go
@@ -1,8 +1,17 @@
 package securewebhook
 
 import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/asn1"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -39,4 +48,56 @@ func TestSecureWebhookGetRequestBody(t *testing.T) {
 	actualJSON, err = GetRequestBody(s)
 	require.NoError(t, err)
 	assert.Equal(t, expectedJSONEnabled, actualJSON, fmt.Sprintf("SecureWebhook.Enable should be '%b', got %b", expectedJSONEnabled, actualJSON))
+}
+
+func TestVerifySignature(t *testing.T) {
+	priKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	require.NoError(t, err)
+
+	timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+
+	payload, err := json.Marshal(map[string]interface{}{
+		"event":      "test_event",
+		"category":   "example_payload",
+		"message_id": "message_id",
+	})
+	require.NoError(t, err)
+
+	h := sha256.New()
+	h.Write([]byte(timestamp))
+	h.Write(payload)
+
+	r, s, err := ecdsa.Sign(rand.Reader, priKey, h.Sum(nil))
+	require.NoError(t, err)
+	sigBytes, err := asn1.Marshal(RS{
+		R: r,
+		S: s,
+	})
+	require.NoError(t, err)
+	signature := base64.StdEncoding.EncodeToString(sigBytes)
+
+	// verifications
+	verified, err := VerifySignature(&priKey.PublicKey, payload, signature, timestamp)
+	require.NoError(t, err)
+	assert.True(t, verified)
+
+	// not valid payload
+	verified, err = VerifySignature(&priKey.PublicKey, []byte("this is not valid payload for the given sign"), signature, timestamp)
+	require.NoError(t, err)
+	assert.False(t, verified)
+
+	// not valid signature
+	verified, err = VerifySignature(&priKey.PublicKey, payload, signature+"causing failure", timestamp)
+	require.Error(t, err)
+	assert.False(t, verified)
+
+	// not valid timestamp
+	verified, err = VerifySignature(&priKey.PublicKey, payload, signature, "invalid timestamp")
+	require.NoError(t, err)
+	assert.False(t, verified)
+
+	// empty timestamp
+	verified, err = VerifySignature(&priKey.PublicKey, payload, signature, "")
+	require.NoError(t, err)
+	assert.False(t, verified)
 }
